c0eb622aec7b70d1aa7ff9ec78dabed530404246
jimbo-yo <jlbaird3+github@gmail.com>
2024-10-09 10:41:42 -0400

    initial commit

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+from typing import List, Annotated
+import models
+from database import engine, sessionmaker
+from api import router
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+class ChoiceBase(BaseModel):
+    choice_text: str
+    is_correct: bool
+    
+class QuestionBase(BaseModel):
+    question_text: str
+    choices: List(ChoiceBase)
+
+    
+app.include_router(router)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
